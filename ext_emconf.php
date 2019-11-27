f956ae9223231e446d39fd9ceb522e18668f8abd
Franz Holzinger <holzinger@ttproducts.de>
2019-11-27 12:59:32 +0100

    * compatibility with TYPO3 10: Add the file Configuration/Extbase/Persistence/Classes.php.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = Array (
     'title' => 'Static Info Tables (da)',
     'description' => 'Danish (da) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.7.0',
+    'version' => '6.7.1',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = Array (
     'author_company' => 'jambage.com',
     'constraints' => array (
         'depends' => array(
-            'typo3' => '8.7.0-9.5.99',
-            'static_info_tables' => '6.7.3-6.7.99',
+            'typo3' => '8.7.0-10.99.99',
+            'static_info_tables' => '6.7.3-6.99.99',
         ),
         'conflicts' => array(
         ),
