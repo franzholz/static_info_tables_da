b78bdc5e399517abeea75c9f9888e8eae6ce3448
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-20 16:07:06 +0100

    * add requirement for the extension addons_em: This is needed for the Extension Manager if the file ext_tables_static+adt.sql has been modified manually by the admin.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (da]',
     'description' => 'Danish (da] language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.7.8',
+    'version' => '6.7.11',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Franz Holzinger',
@@ -14,6 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '8.7.0-11.5.99',
             'static_info_tables' => '6.7.3-11.5.99',
+            'addons_em' => '0.5.0-0.5.99',
         ],
         'conflicts' => [
         ],
